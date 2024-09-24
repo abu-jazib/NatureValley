d92515d43c81e1c0d44c3ba438f871fb736a25c9
abu-jazib <abujazib@neurodude.onmicrosoft.com>
2024-09-24 14:03:06 -0700

    products added

---
+++ assets/js/product-detail-agro-mach.js
@@ -0,0 +1,11 @@
+const products = [  
+    {  
+        name: "Aggrro Machinery",  
+        image: "assets/images/products/pr-1.jpg",  
+        price: 256.00,  
+        category: "indoor",  
+        description1: "A beautiful watering can arrangement with narcissus flowers.",  
+        description2: "Perfect for indoor decoration and plant care."  
+    },  
+      
+];  
---
+++ assets/js/product-detail-plant.js
@@ -0,0 +1,19 @@
+const products = [  
+    {  
+        name: "ALOE VERA – MEDICINAL PLANT",  
+        image: "assets/images/products/plants/pr-1.jpg",  
+        price: 350.00,  
+        category: "indoor",  
+        description1: "Aloe vera is a succulent plant species of the genus Aloe. Having some 500 species, Aloe is widely distributed, and is considered an invasive species in many world regions.",  
+        description2: "An evergreen perennial, it originates from the Arabian Peninsula, but grows wild in tropical, semi-tropical, and arid climates around the world. It is cultivated for commercial products, mainly as a topical treatment used over centuries. The species is attractive for decorative purposes, and succeeds indoors as a potted plant. It is used in many consumer products, including beverages, skin lotion, cosmetics, ointments or in the form of gel for minor burns and sunburns. There is little clinical evidence for the effectiveness or safety of Aloe vera extract as a cosmetic or topical drug.The name derives from Latin as aloe and vera (“true”)"  
+    },
+    {  
+        name: "ANTHURIUM FLAMINGO",  
+        image: "assets/images/products/plants/pr-2.jpg",  
+        price: 6500.00,  
+        category: "indoor",  
+        description1: "Anthurium is a genus of tropical plants known for their distinctive, glossy, heart-shaped leaves and unique, colorful spathe-and-spadix flowers. Commonly called “laceleaf” or “flamingo flower,” Anthuriums are prized as indoor ornamental plants. They thrive in warm, humid conditions and are valued for their striking appearance and long-lasting blooms.",  
+        description2: ""  
+    },   
+     
+];  
